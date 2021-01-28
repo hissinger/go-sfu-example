2935dbf1b766a0f748e34c30caac56d2cf17db6f
loje75 <loje75@gmail.com>
2021-01-28 15:25:18 +0900

    RTCP 처리 기능 개발

--- sfu/go.mod
+++ sfu/go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	github.com/pion/rtcp v1.2.6
+	github.com/pion/rtp v1.6.2
 	github.com/pion/webrtc/v3 v3.0.4
 	github.com/sirupsen/logrus v1.7.0
 )
---
+++ sfu/main.go
@@ -0,0 +1,463 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/pion/rtcp"
+	"github.com/pion/rtp"
+	"github.com/pion/webrtc/v3"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	maxSeqNo       = 1 << 16
+	maxMessageSize = 1024 * 9
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type stream struct {
+	sync.Mutex
+
+	ssrc     uint32
+	receiver *webrtc.RTPReceiver
+
+	clockRate uint32
+	cname     string
+
+	packetCount  uint32
+	lastExpected uint32
+	bitrate      uint64
+	lastReport   int64
+	lastReceived uint32
+	lostRate     float32
+	jitter       float64
+
+	latestTimestampTime int64
+	latestTimestamp     uint32
+
+	lastSRNTPTime      uint64
+	lastSRRTPTime      uint32
+	lastSRRecv         int64 // Represents wall clock of the most recent sender report arrival
+	baseSeqNo          uint16
+	cycles             uint32
+	lastRtcpPacketTime int64 // Time the last RTCP packet was received.
+	lastRtcpSrTime     int64 // Time the last RTCP SR was received. Required for DLSR computation.
+	lastTransit        uint32
+	maxSeqNo           uint16 // The highest sequence number received in an RTP data packet
+}
+
+type Client struct {
+	conn           *websocket.Conn
+	send           chan []byte
+	peerConnection *webrtc.PeerConnection
+
+	receiveStreams map[uint32]*stream
+}
+
+type ICECandidate struct {
+	Type      string                  `json:"type"`
+	Candidate webrtc.ICECandidateInit `json:"candidate"`
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	for {
+		_, byte, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Errorf("error: %v", err)
+			}
+			break
+		}
+
+		var message map[string]interface{}
+
+		if err := json.Unmarshal(byte, &message); err != nil {
+			panic(err)
+		}
+
+		// rececive message
+		// log.Debug(message)
+
+		switch message["type"] {
+		case "offer":
+			c.receiveOffer(string(byte))
+		case "candidate":
+			c.receiveCandidate(string(byte))
+		}
+	}
+}
+
+func (c *Client) writePump() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) receiveOffer(message string) {
+	log.Debug("message: ", message)
+
+	var offer webrtc.SessionDescription
+	if err := json.NewDecoder(strings.NewReader(message)).Decode(&offer); err != nil {
+		panic(err)
+	}
+	if err := c.peerConnection.SetRemoteDescription(offer); err != nil {
+		panic(err)
+	}
+
+	answer, err := c.peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	byte, _ := json.Marshal(answer)
+	log.Debug("answer:", string(byte))
+
+	c.send <- byte
+}
+
+func (c *Client) receiveCandidate(message string) {
+	log.Debug("candidate: ", message)
+
+	var candidate = webrtc.ICECandidateInit{Candidate: message}
+
+	if err := c.peerConnection.AddICECandidate(candidate); err != nil {
+		panic(err)
+	}
+}
+
+func (c *Client) peerConnectionFactory() {
+
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
+
+	pc, err := api.NewPeerConnection(webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+		SDPSemantics: webrtc.SDPSemanticsUnifiedPlan,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = pc.AddTransceiverFromKind(webrtc.RTPCodecTypeVideo)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = pc.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio)
+	if err != nil {
+		panic(err)
+	}
+
+	pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+
+		log.Debug("OnICECandidate:", candidate.ToJSON().Candidate)
+
+		byte, _ := json.Marshal(ICECandidate{
+			Type:      "candidate",
+			Candidate: candidate.ToJSON(),
+		})
+
+		c.send <- byte
+	})
+
+	pc.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		log.Debugf("Connection State has changed %s \n", connectionState.String())
+	})
+
+	pc.OnTrack(func(remoteTrack *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+
+		ssrc := uint32(remoteTrack.SSRC())
+		stream := &stream{
+			receiver:  receiver,
+			ssrc:      ssrc,
+			clockRate: remoteTrack.Codec().ClockRate,
+		}
+
+		log.Println("clock:", remoteTrack.Codec().ClockRate)
+		c.receiveStreams[ssrc] = stream
+
+		go func() {
+			for {
+				log.Debug("bps:", stream.bitrate)
+				stream.bitrate = 0
+				time.Sleep(time.Second * 1)
+			}
+		}()
+
+		go func() {
+			for {
+				time.Sleep(time.Second * 1)
+				rtcp := []rtcp.Packet{c.buildREMBPacket()}
+				pc.WriteRTCP(rtcp)
+
+			}
+		}()
+
+		go func() {
+			for {
+				pkts, _, err := receiver.ReadRTCP()
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				for _, pkt := range pkts {
+					switch pkt.(type) {
+					case *rtcp.SenderReport:
+						c.handleSR(pkt.(*rtcp.SenderReport))
+					case *rtcp.SourceDescription:
+						c.handleSDES(pkt.(*rtcp.SourceDescription))
+					default:
+						log.Println("unknown RTCP Type:", pkt)
+					}
+				}
+			}
+		}()
+
+		go func() {
+			pkt := make([]byte, 1400)
+			for {
+				i, _, readErr := remoteTrack.Read(pkt)
+				if readErr != nil {
+					log.Fatalln(readErr)
+				}
+				arrivalTime := time.Now().UnixNano()
+
+				seq := binary.BigEndian.Uint16(pkt[2:4])
+				if stream.packetCount == 0 {
+					stream.baseSeqNo = seq
+					stream.maxSeqNo = seq
+					stream.lastReport = arrivalTime
+				} else if (seq-stream.maxSeqNo)&0x8000 == 0 {
+					if seq < stream.maxSeqNo {
+						stream.cycles += maxSeqNo
+					}
+					stream.maxSeqNo = seq
+				}
+				stream.packetCount++
+				stream.bitrate += (uint64(i) * 8)
+
+				var p rtp.Packet
+				if err := p.Unmarshal(pkt); err != nil {
+					return
+				}
+
+				if (stream.latestTimestampTime == 0) || IsLaterTimestamp(p.Timestamp, stream.latestTimestamp) {
+					stream.latestTimestamp = p.Timestamp
+					stream.latestTimestampTime = arrivalTime
+				}
+
+				arrival := uint32(arrivalTime / 1e6 * int64(stream.clockRate/1e3))
+				transit := arrival - p.Timestamp
+
+				if stream.lastTransit != 0 {
+					d := int32(transit - stream.lastTransit)
+					if d < 0 {
+						d = -d
+					}
+					stream.jitter += (float64(d) - stream.jitter) / 16
+				}
+
+				stream.lastTransit = transit
+			}
+		}()
+	})
+
+	c.peerConnection = pc
+}
+
+// IsLaterTimestamp returns true if timestamp1 is later in time than timestamp2 factoring in timestamp wrap-around
+func IsLaterTimestamp(nowTimestamp uint32, latestTimestamp uint32) bool {
+	if nowTimestamp > latestTimestamp {
+		if IsTimestampWrapAround(latestTimestamp, nowTimestamp) {
+			return false
+		}
+		return true
+	}
+	if IsTimestampWrapAround(nowTimestamp, latestTimestamp) {
+		return true
+	}
+	return false
+}
+
+// IsTimestampWrapAround returns true if wrap around happens from timestamp1 to timestamp2
+func IsTimestampWrapAround(nowTimestamp uint32, latestTimestamp uint32) bool {
+	return (nowTimestamp&0xC000000 == 0) && (latestTimestamp&0xC000000 == 0xC000000)
+}
+
+func (c *Client) handleSR(sr *rtcp.SenderReport) {
+	for _, ssrc := range sr.DestinationSSRC() {
+		// log.Println("receive SR:", ssrc)
+		s, ok := c.receiveStreams[ssrc]
+		if ok == true {
+			// log.Println(s.receiver.Track().Kind())
+			// log.Println(sr.String())
+			s.SetSenderReportData(sr.RTPTime, sr.NTPTime)
+
+			rtcp := []rtcp.Packet{
+				&rtcp.ReceiverReport{
+					Reports: []rtcp.ReceptionReport{s.buildReceptionReport()},
+				},
+			}
+			c.peerConnection.WriteRTCP(rtcp)
+		}
+	}
+}
+
+func (s *stream) SetSenderReportData(rtpTime uint32, ntpTime uint64) {
+	s.Lock()
+	s.lastSRRTPTime = rtpTime
+	s.lastSRNTPTime = ntpTime
+	s.lastSRRecv = time.Now().UnixNano()
+	s.Unlock()
+}
+
+func (c *Client) handleSDES(sdes *rtcp.SourceDescription) {
+	for _, chunk := range sdes.Chunks {
+		for _, item := range chunk.Items {
+			if item.Type == rtcp.SDESCNAME {
+				s := c.receiveStreams[chunk.Source]
+				s.Lock()
+				defer s.Unlock()
+				s.cname = item.Text
+			}
+		}
+	}
+}
+
+func (s *stream) buildReceptionReport() rtcp.ReceptionReport {
+	extMaxSeq := s.cycles | uint32(s.maxSeqNo)
+	expected := extMaxSeq - uint32(s.baseSeqNo) + 1
+	lost := expected - s.packetCount
+	if s.packetCount == 0 {
+		lost = 0
+	}
+	expectedInterval := expected - s.lastExpected
+	s.lastExpected = expected
+
+	receivedInterval := s.packetCount - s.lastReceived
+	s.lastReceived = s.packetCount
+
+	lostInterval := expectedInterval - receivedInterval
+
+	s.lostRate = float32(lostInterval) / float32(expectedInterval)
+	var fracLost uint8
+	if expectedInterval != 0 && lostInterval > 0 {
+		fracLost = uint8((lostInterval << 8) / expectedInterval)
+	}
+	var dlsr uint32
+
+	if s.lastSRRecv != 0 {
+		delayMS := uint32((time.Now().UnixNano() - s.lastSRRecv) / 1e6)
+		dlsr = (delayMS / 1e3) << 16
+		dlsr |= (delayMS % 1e3) * 65536 / 1000
+	}
+
+	rr := rtcp.ReceptionReport{
+		SSRC:               s.ssrc,
+		FractionLost:       fracLost,
+		TotalLost:          lost,
+		LastSequenceNumber: extMaxSeq,
+		Jitter:             uint32(s.jitter),
+		LastSenderReport:   uint32(s.lastSRNTPTime >> 16),
+		Delay:              dlsr,
+	}
+
+	return rr
+}
+
+func (c *Client) buildREMBPacket() *rtcp.ReceiverEstimatedMaximumBitrate {
+	br := uint64(2 * 1024 * 1024)
+
+	for _, r := range c.peerConnection.GetReceivers() {
+		if r.Track().Kind() == webrtc.RTPCodecTypeVideo {
+			return &rtcp.ReceiverEstimatedMaximumBitrate{
+				Bitrate: br,
+				SSRCs:   []uint32{uint32(r.Track().SSRC())},
+			}
+		}
+	}
+	return nil
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	client := &Client{
+		conn:           c,
+		send:           make(chan []byte, 256),
+		receiveStreams: map[uint32]*stream{},
+	}
+
+	client.peerConnectionFactory()
+
+	go client.writePump()
+	go client.readPump()
+}
+
+var m *webrtc.MediaEngine
+
+func main() {
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	m = &webrtc.MediaEngine{}
+	m.RegisterDefaultCodecs()
+
+	http.HandleFunc("/ws", serveWs)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
